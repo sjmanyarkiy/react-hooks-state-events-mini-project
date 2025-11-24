5278fa62ecca7eaa194cdb670c0a85cea91f1faf
commit-panda <sjmanyarkiy@gmail.com>
2025-11-24 14:19:45 +0300

    completed lab

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("Code");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onTaskFormSubmit({ text, category });
+    setText("");
+    setCategory("Code");
+  }
+
+  const options = categories
+    .filter((cat) => cat !== "All")
+    .map((cat) => (
+      <option key={cat} value={cat}>
+        {cat}
+      </option>
+    ));
+
   return (
     <form className="new-task-form">
       <label>
@@ -11,6 +29,7 @@ function NewTaskForm() {
         Category
         <select name="category">
           {/* render <option> elements for each category here */}
+          {options}
         </select>
       </label>
       <input type="submit" value="Add task" />
